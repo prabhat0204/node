5dfdb26364069a293e7603b8a7215fd78b69d38c
Kirti <succeedSerivces28@gmail.com>
2026-07-17 21:27:59 +0530

    d

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ console.log('VERIFY_TOKEN:', verifyToken);
 // Route for GET requests
 app.get('/', (req, res) => {
   console.log('WEBHOOK VERIFIED111');
-  console.log('req.query:', req.body);
+  console.log('req.query:', req.query);
 
   const { 'hub.mode': mode, 'hub.challenge': challenge, 'hub.verify_token': token } = req.query;
 
